14a8c490b1b3b5209bb9bc35ff4f04e8f30e5eab
John Patrick <john.p.r.manarang@accenture.com>
2020-10-30 10:55:35 +0800

    angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sampleappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sampleappApp
+ */
+angular.module('sampleappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
